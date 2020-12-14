f045d39bff49cf531ee782c170aaf5ed1fbfdc58
friendbear <friendbear@users.noreply.github.com>
2020-12-14 11:57:41 +0900

    add recorder

--- app/src/main/java/com/example/tutorial/WeatherForecast.kt
+++ app/src/main/java/com/example/tutorial/WeatherForecast.kt
@@ -2,12 +2,24 @@ package com.example.tutorial
 
 
 class WeatherForecast (val satellite: Satellite){
+    private val recorder = WeatherRecorder()
 
     fun shouldBringUmbrella(): Boolean {
         return when (satellite.getWeather()) {
             "雨" -> true
             else -> false
         }
+    }
+
+    fun recordCurrentWeather() {
+        val weather = satellite.getWeather()
+        recorder.record(weather)
+    }
+}
+
+class WeatherRecorder {
+    fun record(weather: String): Unit{
+        // DB
     }
 }
 
