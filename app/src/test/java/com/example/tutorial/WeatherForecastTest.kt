207bf8822beecabd5bcc0d6d20c5f6e213d2100d
friendbear <friendbear@users.noreply.github.com>
2020-12-14 12:23:24 +0900

    add test

--- app/src/test/java/com/example/tutorial/WeatherForecastTest.kt
+++ app/src/test/java/com/example/tutorial/WeatherForecastTest.kt
@@ -8,8 +8,21 @@ class WeatherForecastTest {
 
     @Test
     fun getWeatherが雨ならshouldBringUmbrellaがtrueを返す() {
-        val sut = WeatherForecast(SatelliteStub("雨"))
+        val sut = WeatherForecast(SatelliteStub("雨"), WeatherRecorder())
         val actual = sut.shouldBringUmbrella()
         assertThat(actual).isTrue()
+    }
+
+    @Test
+    fun recorderクラスにrecordされている() {
+        val recorder = MockWeatherRecorder()
+        val sut = WeatherForecast(SatelliteStub("晴れ"), recorder)
+
+        sut.recordCurrentWeather()
+        val isCalled = recorder.isCalled
+        assertThat(isCalled).isTrue()
+
+        val weather = recorder.currentWeather
+        assertThat(weather).isIn("晴れ", "曇り", "雨")
     }
 }
